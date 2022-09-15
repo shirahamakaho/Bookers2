ea9ff390edce8f111e01b6ed371a033d0a8c701d
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-09-15 09:46:29 +0000

    rest

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,15 @@
+class BooksController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_15_072045) do
+ActiveRecord::Schema.define(version: 2022_09_15_094411) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_09_15_072045) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

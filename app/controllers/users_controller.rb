d99f15740c4187790a2fd77282917ac9ee914819
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-09-15 14:09:17 +0000

    sleep

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
   end
+
 end

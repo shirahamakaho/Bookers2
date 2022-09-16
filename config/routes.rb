17a2e93599514af5c4803687d9558d83eea062ad
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-09-16 00:31:15 +0000

    yah

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about',as:"about"
   devise_for :users
   resources:users,only:[:index,:show,:edit]
-  resources:books,only:[:create,:index,:show,:edit]
+  resources:books,only:[:create,:index,:show,:edit,:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

5c7960a484a95c408bc5c85054fb947fcd253a41
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-09-17 08:06:55 +0000

    rest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action:configure_permitted_parameters,if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    user_path(current_user.id)
+  end
+
   def after_sign_in_path_for(resource)
     user_path(current_user.id)
   end
@@ -12,6 +16,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:email,:name])
   end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "You have created book successfully."
+    else
+      render :index
+    end
   end
 
   def show
@@ -30,8 +34,10 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "You have updated book successfully."
+    end
   end
 
   def destroy

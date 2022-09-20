2d9c049af33d3fa5ee9616a9f90f597b37115b57
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-09-20 09:57:51 +0000

    [Finish]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user.id)
+      redirect_to user_path(current_user.id)
       flash[:notice] = "You have updated user successfully."
     else
       render:edit
